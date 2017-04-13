6e43deb8822eecad12959dd0038faac480f8e36d
Charles Wall <CharlesTWall3@gmail.com>
2017-04-13 16:20:13 +0000

    add script to bin; handle npm linking to shell script

--- ssh-selection.sh
+++ ssh-selection.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-pushd `dirname $0`
+# from http://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+function getSourceDirectory() {
+  SOURCE="${BASH_SOURCE[0]}"
+  while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  done
+  printf "$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+}
+
+pushd `getSourceDirectory`
 
 name=$1
 uuid=$(node uuid.js)
 
+set -e
 node index.js $name $uuid
 
 ip=`cat $uuid`
